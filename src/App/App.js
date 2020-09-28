175dd0710c91497e632bb1fbbc422e37801c5e8e
Dan Reardon <dreardon1021@gmail.com>
2020-09-28 11:38:39 -0600

    Create App folder and Move files

---
+++ src/App/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentQuiz: []
+    }
+  }
+
+  render() {
+    return(
+      <main>
+        
+      </main>
+    )
+  }
+}
+
+export default App;
