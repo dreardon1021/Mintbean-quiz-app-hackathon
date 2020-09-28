2d71307d9ee74f2e351e535a982f5f3e0ccbb96c
Dan Reardon <dreardon1021@gmail.com>
2020-09-28 14:59:57 -0600

    Edit route and add key

--- src/QuizSelectionCard/QuizSelectionCard.js
+++ src/QuizSelectionCard/QuizSelectionCard.js
@@ -10,9 +10,9 @@ const QuizSelectionCard = ({ quiz }) => {
   return (
     <article className="quiz-card">
       <h3 className="quiz-card-name">{quiz.name}</h3>
-      <img className="quiz-card-image" src={quiz.image} alt={quiz.name} />
+      <img key={quiz.name} className="quiz-card-image" src={quiz.image} alt={quiz.name} />
       <p className="quiz-card-text">Quiz Length: {quiz.questions.length} questions</p>
-      <NavLink className="text-decoration-none" to={"/quiz-selection/" + quiz.name}><Button className="quiz-button text-decoration-none" variant="contained" color="primary">Select Quiz</Button></NavLink>
+      <NavLink className="text-decoration-none" to={`/quiz-selection/${quiz.name}`}><Button className="quiz-button text-decoration-none" variant="contained" color="primary">Select Quiz</Button></NavLink>
     </article>
   )
 }
